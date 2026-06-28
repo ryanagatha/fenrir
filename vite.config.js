3618b251452d963f8408cad97eeb6951d3675e0a
ryanagatha <ryanagatha01@gmail.com>
2026-06-28 12:11:40 +0700

    Add built docs/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/tptptp/',
+  build: { outDir: 'docs' },
   server: {
     fs: {
       allow: [
